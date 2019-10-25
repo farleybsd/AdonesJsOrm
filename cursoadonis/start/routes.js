7ea15d3ce7b44cd134134e959b738add0e8a15cf
farleybsd <farley.t.i@hotmail.com>
2019-10-25 11:31:42 -0300

    Primeiras Rotas

---
+++ cursoadonis/start/routes.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.group(() =>{
+
+Route.get('/', () => {
+  return { greeting: 'Hello world in JSON' }
+})
+
+Route.get('/postagem/:id',({params})=>{
+  return {mensagem:'Retorno da rota postagem com o parametro - ' + params.id}
+
+})
+
+Route.post('/postagem', () => {
+  return { mensagem: 'Retorno da rota postagem!' }
+})
+
+Route.put('/postagem', () => {
+  return { mensagem: 'Retorno da rota postagem!' }
+})
+
+Route.delete('/postagem', () => {
+  return { mensagem: 'Retorno da rota postagem!' }
+})
+
+})
